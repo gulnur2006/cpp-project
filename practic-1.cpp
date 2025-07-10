e91006800fd4011b4c85f3037e892f7f903107d2
gulnur2006 <gulnurannageldiyewa@gmail.com>
2025-07-10 08:30:37 +0300

    Add files via upload

--- practic-1.cpp
+++ practic-1.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-const int MAX_LIMIT = 1000;
-
+const int MAX_LIMIT = 100;
 
 bool isPrime(int n) {
     if (n <= 1) return false;
@@ -15,7 +14,6 @@ bool isPrime(int n) {
     }
     return true;
 }
-
 
 bool contains(const int arr[], int size, int value) {
     for (int i = 0; i < size; ++i) {
@@ -39,44 +37,39 @@ int main() {
 
     int parabolicPrimes[MAX_LIMIT];
     int parabolicCount = 0;
-
     
     for (int i = 0; i < primeCount; ++i) {
         for (int j = 0; j < primeCount; ++j) {
             int q = primes[i];
             int r = primes[j];
             int p = q * q + r + 1;
-
-            
+                        
             if (p < MAX_LIMIT && isPrime(p)) {
-                
+              
                 if (!contains(parabolicPrimes, parabolicCount, p)) {
                     parabolicPrimes[parabolicCount++] = p;
                 }
             }
         }
     }
-
-    
+       
     for (int i = 0; i < parabolicCount - 1; ++i) {
         for (int j = 0; j < parabolicCount - i - 1; ++j) {
             if (parabolicPrimes[j] > parabolicPrimes[j + 1]) {
-                
                 int temp = parabolicPrimes[j];
                 parabolicPrimes[j] = parabolicPrimes[j + 1];
                 parabolicPrimes[j + 1] = temp;
             }
         }
     }
-
-    
-    cout << "Unique Parabolic Primes:" << endl;
+        
+    cout << "Parabolic Primes:" << endl;
     for (int i = 0; i < parabolicCount; ++i) {
         cout << parabolicPrimes[i] << " ";
     }
     cout << endl;
 
-    cout << "Count of unique Parabolic Primes: " << parabolicCount << endl;
+    cout << "Count of Parabolic Primes: " << parabolicCount << endl;
 
     return 0;
 }
